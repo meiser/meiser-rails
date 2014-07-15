724b384f641677886acae70d80c958499752b00a
Stephan Keller <MiStK@gmx.de>
2014-07-15 08:23:55 +0200

    Update informix.rb
    fix changed access to database.yml file in rails 4.x

--- lib/meiser-rails/informix.rb
+++ lib/meiser-rails/informix.rb
@@ -11,7 +11,9 @@ module MeiserRails
     def baan_available?
      begin
       #MeiserRails::Db.connection.nil? ? false : true
-      IBM_DB.connect(Rails.application.config.meiser.baan, "", "").nil? ? false : true
+      
+      yml_conf = Rails.version.starts_with?("4") ? Rails.application.config.database_configuration["baan"] : Rails.application.config.meiser.baan
+      IBM_DB.connect(yml_conf, "", "").nil? ? false : true
      rescue
       puts "Connection failed: #{IBM_DB::conn_errormsg}"
      end
@@ -23,7 +25,8 @@ module MeiserRails
 
       begin
        #conn = MeiserRails::Db.connection
-       conn = IBM_DB.connect(Rails.application.config.meiser.baan, "", "")
+       yml_conf = Rails.version.starts_with?("4") ? Rails.application.config.database_configuration["baan"] : Rails.application.config.meiser.baan
+       conn = IBM_DB.connect(yml_conf, "", "")
 
        if stmt = IBM_DB.prepare(conn,query)
         IBM_DB.execute(stmt, params)
@@ -45,7 +48,8 @@ module MeiserRails
     def update_baan(query, params = [])
       begin
         params = informix_date_conversion(params)
-        conn = IBM_DB.connect(Rails.application.config.meiser.baan, "", "")
+        yml_conf = Rails.version.starts_with?("4") ? Rails.application.config.database_configuration["baan"] : Rails.application.config.meiser.baan
+        conn = IBM_DB.connect(yml_conf, "", "")
         #conn = MeiserRails::Db.connection
 	stmt = IBM_DB.prepare(conn, query)
         return IBM_DB.execute(stmt,params)
